c97fda987477de0fcc5dd3f1e18c3ddf714d59d1
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2021-06-17 23:48:26 +0530

    Added conv architecture 1

---
+++ data/architectures/model1/model1.py
@@ -0,0 +1,44 @@
+import numpy as np
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D,Activation,MaxPooling2D
+from keras.layers import Concatenate
+from keras import Input
+from keras.models import Model, Sequential
+
+def get_model():
+    data = np.load('./data.npy')
+    input_shape = data.shape[1:]  # 50,50,1
+    inp = Input(shape=input_shape)
+    convs = []
+
+    parrallel_kernels = [3, 5, 7]
+
+    for k in range(len(parrallel_kernels)):
+        conv = Conv2D(128, parrallel_kernels[k], padding='same', activation='relu',
+                      input_shape=input_shape, strides=1)(inp)
+        convs.append(conv)
+
+    out = Concatenate()(convs)
+    conv_model = Model(inputs=inp, outputs=out)
+
+    model = Sequential()
+    model.add(conv_model)
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(2, input_dim=128, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    return model
