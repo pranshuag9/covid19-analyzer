179af737bff43f04d013dc741bd2dddcc6c994c7
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2021-06-19 14:49:50 +0530

    Added custom logging feature to log backend requests

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
+import logging
+from pathlib import Path
+
+import custom_logging_config
+
 img_size = 100
+
 label_dict = {0: 'Covid19 Negative', 1: 'Covid19 Positive'}
+
 MODEL_PATH = 'model/model-015.model'
+
+log_dir_path = Path(__file__).parent.absolute()
+
+log: logging.Logger = custom_logging_config.get_logger("chest-xray-analyzer", log_dir_path)
