b91fa590421eb9331b1a02e49d55331f6f39aa6a
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2021-06-20 02:44:30 +0530

    Fixed custom logging feature to log backend requests

--- custom_logging_config.py
+++ custom_logging_config.py
@@ -3,7 +3,33 @@ from logging.config import dictConfig
 
 
 def get_logger(logger, dir_path) -> logging.Logger:
-	logging_config = {"version": 1, "disable_existing_loggers": False, "formatters": {"default": {"format": "%(asctime)s: [%(filename)s:%(lineno)s in %(funcName)s()] %(message)s",}, "info": {"format": "[%(asctime)s]: %(message)s", "datefmt": "%H:%M:%S"}, "handlers": {"debugfilehandler": {'level': 'DEBUG', 'class': 'logging.FileHandler', 'filename': dir_path / 'log.log', 'formatter': 'default'}}}, "loggers": {"chest-xray-analyzer": {'handlers': ['debugfilehandler'], 'level': 'DEBUG', 'propogate': True,}}}
+	logging_config = {
+		"version": 1,
+		"disable_existing_loggers": False,
+		"formatters": {
+			"default": {
+				"format": "%(asctime)s: [%(filename)s:%(lineno)s in %(funcName)s()] %(message)s",
+			},
+			"info": {
+				"format": "[%(asctime)s]: %(message)s", "datefmt": "%H:%M:%S"
+			},
+		},
+		"handlers": {
+			'debugfilehandler': {
+				'level': 'DEBUG',
+				'class': 'logging.FileHandler',
+				'filename': dir_path / 'log.log',
+				'formatter': 'default'
+			},
+		},
+		"loggers": {
+			"chest-xray-analyzer": {
+				'handlers': ['debugfilehandler'],
+				'level': 'DEBUG',
+				'propogate': True,
+			},
+		}
+	}
 	dictConfig(logging_config)
 	custom_log = logging.getLogger(logger)
 	return custom_log
