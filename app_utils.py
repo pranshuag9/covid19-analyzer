8be14935d346d2991bdd97e093cc7da5e589dff8
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2021-06-20 03:27:46 +0530

    Added get PIL image instance function which accepts raw image from frontend

--- app_utils.py
+++ app_utils.py
@@ -1,18 +1,33 @@
+import base64
+import io
+
 import cv2
 import numpy as np
+from PIL import Image
 
-from config import img_size
+from config import img_size, log
 
 
 def preprocess(img):
-	img = np.array(img)
+	log.info("Intiating image preprocessing")
+	img: np.core.multiarray = np.array(img)
 
-	if (img.ndim == 3):
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	if img.ndim == 3:
+		gray: np.core.multiarray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	else:
-		gray = img
+		gray: np.core.multiarray = img
 
 	gray = gray / 255
 	resized = cv2.resize(gray, (img_size, img_size))
 	reshaped = resized.reshape(1, img_size, img_size)
+	log.info(f"Reshaped image to: {img_size}")
 	return reshaped
+
+
+def get_pil_image_instance(encoded_img):
+	decoded_img = base64.b64decode(encoded_img)
+
+	dataBytesIO = io.BytesIO(decoded_img)
+	dataBytesIO.seek(0)
+
+	return Image.open(dataBytesIO)
