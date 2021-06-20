2f3a41bdbf2256e9bca76dfd335207bdd4d9f0eb
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2021-06-21 01:22:02 +0530

    Added load_data functions

--- app_utils.py
+++ app_utils.py
@@ -1,12 +1,15 @@
 import base64
 import io
+import os
 from os import path, walk
 
 import cv2
 import numpy as np
 from PIL import Image
+from tensorflow.python.keras.utils.np_utils import to_categorical
 
-from config import img_size, log
+from config import img_size, log, label_dict_val_category, label_dict_category_val, \
+	npy_data_path, npy_target_path
 
 
 def get_extra_files_to_watch_for_reload():
@@ -19,6 +22,56 @@ def get_extra_files_to_watch_for_reload():
 				if path.isfile(filename):
 					extra_files.append(filename)
 	return extra_files
+
+
+def save_images_to_npy(data, target, data_filename, target_filename):
+	np.save(data_filename, data)
+	np.save(target_filename, target)
+
+
+def load_data_from_files(npy_data_path, npy_target_path):
+	data = np.load(npy_data_path)
+	target = np.load(npy_target_path)
+	return data, target
+
+
+def load_data_from_disk(data_dir):
+	categories = os.listdir(data_dir)
+	img_size = 100
+	data, target = [], []
+
+	for category in categories:
+		folder_path = os.path.join(data_dir, category)
+		img_names = os.listdir(folder_path)
+
+		for img_name in img_names:
+			img_path = os.path.join(folder_path, img_name)
+			img = cv2.imread(img_path)
+
+			try:
+				gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+				resized = cv2.resize(gray, (img_size, img_size))
+				data.append(resized)
+				target.append(label_dict_category_val[category])
+			except Exception as e:
+				log.info(f"Error occurred while preprocessing image, {str(e)}")
+
+	data = np.array(data) / 255.0
+	data = np.reshape(a=data, newshape=(data.shape[0], img_size, img_size, 1))
+	target = np.array(target)
+	new_target = to_categorical(target)
+
+	data_file, target_file = npy_data_path.split(".")[0], npy_target_path.split(".")[0]
+
+	save_images_to_npy(
+		data=data,
+		target=new_target,
+		data_filename=data_file,
+		target_filename=target_file
+	)
+	npy_data, npy_target = load_data_from_files(npy_data_path, npy_target_path)
+
+	return npy_data, npy_target
 
 
 def preprocess(img):
