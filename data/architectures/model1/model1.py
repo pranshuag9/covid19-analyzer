3c18fb367cbcffdea097f5be0be4baf9ed6f5ad3
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2021-06-20 21:53:45 +0530

    Improved code visibility

--- data/architectures/model1/model1.py
+++ data/architectures/model1/model1.py
@@ -1,17 +1,15 @@
-import numpy as np
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D,Activation,MaxPooling2D
 from keras.layers import Concatenate
 from keras import Input
 from keras.models import Model, Sequential
 
-def get_model():
-    data = np.load('./data.npy')
-    input_shape = data.shape[1:]  # 50,50,1
+def get_model(input_shape):
     inp = Input(shape=input_shape)
     convs = []
     parrallel_kernels = [3, 5, 7]
 
+    # Building the first layer of architecture
     for kernel_size in parrallel_kernels:
         conv = Conv2D(
             filters=128,
@@ -27,23 +25,23 @@ def get_model():
     conv_model = Model(inputs=inp, outputs=out)
 
     model = Sequential()
-    model.add(conv_model)
+    model.add(layer=conv_model)
 
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3)))
+    model.add(Activation(activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
+    model.add(Conv2D(filters=32, kernel_size=(3, 3)))
+    model.add(Activation(activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Flatten())
-    model.add(Dropout(0.5))
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(2, input_dim=128, activation='softmax'))
+    model.add(Dropout(rate=0.5))
+    model.add(Dense(units=128, activation='relu'))
+    model.add(Dropout(rate=0.5))
+    model.add(Dense(units=64, activation='relu'))
+    model.add(Dropout(rate=0.5))
+    model.add(Dense(units=2, input_dim=128, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     return model
