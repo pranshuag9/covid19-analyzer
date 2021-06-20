7bf6f624de9a497820963f779346c8a5b8ecd36c
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2021-06-21 01:22:31 +0530

    Updated config.py for static variables

--- config.py
+++ config.py
@@ -1,13 +1,24 @@
+import datetime
 import logging
 from pathlib import Path
+
+import pytz as pytz
 
 import custom_logging_config
 
 img_size = 100
 
-label_dict = {0: 'Covid19 Negative', 1: 'Covid19 Positive'}
+label_dict_category_val = {'Covid19_Negative': 0, 'Covid19_Positive': 1}
+
+label_dict_val_category = {0: 'Covid19_Negative', 1: 'Covid19_Positive'}
 
 MODEL_PATH = 'models/model-015.model'
+current_timestamp = datetime.datetime.now(tz=pytz.timezone("Asia/Kolkata"))
+MODEL_CHECKPOINT_PATH = f'models/checkpoints/{current_timestamp}'+'model-{epoch:03d}.model'
+
+DATASET_DIR = "data/dataset"
+
+npy_data_path, npy_target_path = "data.npy", "target.npy"
 
 log_dir_path = Path(__file__).parent.absolute()
 
